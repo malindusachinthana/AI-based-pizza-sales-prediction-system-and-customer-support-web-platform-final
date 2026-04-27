d7cccfdf926c6b6ef3311f5d07ea2fb3b2f1d24d
Malindu Sachinthana <malinduuus@gmail.com>
2026-04-28 00:08:45 +0530

    update-register-imports

--- frontend/src/pages/register.jsx
+++ frontend/src/pages/register.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import axios from 'axios';
 import '../style/register.css';
 
 const Register = () => {

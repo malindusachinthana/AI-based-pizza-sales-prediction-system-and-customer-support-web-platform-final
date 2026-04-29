bf3c5eec51ca7af527bbc21732779f4f3f82a7ab
Malindu Sachinthana <malinduuus@gmail.com>
2026-04-29 23:02:26 +0530

    create-footer-jsx

---
+++ frontend/src/components/footer.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style/footer.css';
+import logoCrust from '../assets/OvenzaCrust logo.png';
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+
+      {/* ── Logo ── */}
+      <div className="footer-logo">
+        <img src={logoCrust} alt="OvenZa Crust" />
+        <span>OvenZa</span>
+      </div>
+
+      {/* ── Links ── */}
+      <ul className="footer-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/gallery">Gallery</Link></li>
+        <li><Link to="/menu">Menu</Link></li>
+        <li><Link to="/special-offers">Special Offers</Link></li>
+        <li><Link to="/about">About Us</Link></li>
+      </ul>
+
+      {/* ── Copyright ── */}
+      <p className="footer-copy">
+        © 2026 OvenZa. All Rights Reserved<br />
+        <span style={{ fontSize: '0.7rem', opacity: 0.6 }}>
+          By, Malindu Sachinthana
+        </span>
+      </p>
+
+    </footer>
+  );
+}

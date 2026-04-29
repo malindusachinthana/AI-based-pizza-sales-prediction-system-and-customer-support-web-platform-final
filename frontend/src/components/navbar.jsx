53bb289d12c889eee090424268171d8ce2052996
Malindu Sachinthana <malinduuus@gmail.com>
2026-04-29 23:03:05 +0530

    create-navbar-jsx

---
+++ frontend/src/components/navbar.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import '../style/navbar.css';
+import logo from '../assets/OvenZlogo.png';
+

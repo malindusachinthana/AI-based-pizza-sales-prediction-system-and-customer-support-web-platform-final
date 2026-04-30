24ce1adfe723951574f3a5828d0430b57e30e5c9
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-01 03:42:46 +0530

    update-App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,6 +9,7 @@ import CustomerMenu       from './pages/client/CustomerMenu.jsx';
 
 // Admin Pages
 import AdminDashboard from './pages/admin/adminDashboard.jsx';
+import SalesForecast from './pages/admin/SalesForecast';
 
 function App() {
   return (
@@ -19,10 +20,12 @@ function App() {
           <Route path="/"         element={<CustomerHome />} />
           <Route path="/login"    element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/menu"     element={<CustomerMenu />} /> {/* ✅ Added */}
+          <Route path="/menu"     element={<CustomerMenu />} /> 
+          
 
           {/* Admin Routes */}
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
+          <Route path="/admin/forecast" element={<SalesForecast />} />
         </Routes>
       </div>
     </Router>

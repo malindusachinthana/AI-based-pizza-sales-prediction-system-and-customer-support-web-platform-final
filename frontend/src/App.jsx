90503e50cb7e15384dd466d4a03c35d897a741cf
Malindu Sachinthana <malinduuus@gmail.com>
2026-04-30 16:48:17 +0530

    add-customermenu-to-App.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,24 +5,24 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CustomerHome   from './pages/client/CustomerHome.jsx';
 import Login          from './pages/login.jsx';
 import Register       from './pages/register.jsx';
+import CustomerMenu       from './pages/client/CustomerMenu.jsx';
 
 // Admin Pages
-import AdminDashboard from './pages/admin/adminDashboard.jsx'; // ✅ Added
+import AdminDashboard from './pages/admin/adminDashboard.jsx';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
-
           {/* Client Routes */}
           <Route path="/"         element={<CustomerHome />} />
           <Route path="/login"    element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/menu"     element={<CustomerMenu />} /> {/* ✅ Added */}
 
           {/* Admin Routes */}
-          <Route path="/admin-dashboard" element={<AdminDashboard />} /> {/* ✅ Added */}
-
+          <Route path="/admin-dashboard" element={<AdminDashboard />} />
         </Routes>
       </div>
     </Router>

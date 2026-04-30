048dac80db1f4a8a85767b890e70ba18d026910c
Malindu Sachinthana <malinduuus@gmail.com>
2026-04-30 19:50:56 +0530

    create-customerMenu-jsx

---
+++ frontend/src/pages/client/CustomerMenu.jsx
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+import '../../style/CustomerMenu.css';
+

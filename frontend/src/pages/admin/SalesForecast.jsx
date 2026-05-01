f2a5248272e1b7f832678c236b0fe521728bb05b
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-01 23:45:01 +0530

    connect-api-sf.jsx

--- frontend/src/pages/admin/SalesForecast.jsx
+++ frontend/src/pages/admin/SalesForecast.jsx
@@ -6,3 +6,13 @@ import {
   ResponsiveContainer, Legend
 } from 'recharts';
 import '../../style/SalesForecast.css';
+
+const API = 'http://localhost:5001';
+
+const CATEGORY_COLORS = {
+  Chicken: '#2a7a8a',
+  Classic: '#c8872a',
+  Supreme: '#7a5a8a',
+  Veggie : '#5a8a3a',
+};
+

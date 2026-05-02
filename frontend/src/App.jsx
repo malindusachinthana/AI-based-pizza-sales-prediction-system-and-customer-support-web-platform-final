c171fed805b23025342a1e8e992c1faed5b48f49
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-02 23:40:04 +0530

    update-imports-App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,14 +2,16 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Client Pages
-import CustomerHome   from './pages/client/CustomerHome.jsx';
-import Login          from './pages/login.jsx';
-import Register       from './pages/register.jsx';
-import CustomerMenu       from './pages/client/CustomerMenu.jsx';
+import CustomerHome      from './pages/client/CustomerHome.jsx';
+import Login             from './pages/login.jsx';
+import Register          from './pages/register.jsx';
+import CustomerMenu      from './pages/client/CustomerMenu.jsx';
+import Cart              from './pages/client/cart.jsx';
+import OrderConfirmation from './pages/client/OrderConfirmation.jsx';
 
 // Admin Pages
 import AdminDashboard from './pages/admin/adminDashboard.jsx';
-import SalesForecast from './pages/admin/SalesForecast';
+import SalesForecast  from './pages/admin/SalesForecast';
 
 function App() {
   return (
@@ -17,15 +19,16 @@ function App() {
       <div className="App">
         <Routes>
           {/* Client Routes */}
-          <Route path="/"         element={<CustomerHome />} />
-          <Route path="/login"    element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/menu"     element={<CustomerMenu />} /> 
-          
+          <Route path="/"                   element={<CustomerHome />} />
+          <Route path="/login"              element={<Login />} />
+          <Route path="/register"           element={<Register />} />
+          <Route path="/menu"               element={<CustomerMenu />} />
+          <Route path="/cart"               element={<Cart />} />              
+          <Route path="/order-confirmation" element={<OrderConfirmation />} />
 
           {/* Admin Routes */}
           <Route path="/admin-dashboard" element={<AdminDashboard />} />
-          <Route path="/admin/forecast" element={<SalesForecast />} />
+          <Route path="/admin/forecast"  element={<SalesForecast />} />
         </Routes>
       </div>
     </Router>

78d8ea9830070d129762866edb25b82ed47efc21
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-02 23:39:28 +0530

    update-server.js

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,8 @@ const path        = require('path');
 const connectDB   = require('./config/db');
 const authRoutes  = require('./routes/authRoutes');
 const adminRoutes = require('./routes/adminRoutes');
-const pizzaRoutes = require('./routes/pizzaRoutes'); // ✅ Added
+const pizzaRoutes = require('./routes/pizzaRoutes');
+const orderRoutes = require('./routes/orderRoutes');
 
 dotenv.config();
 connectDB();
@@ -16,7 +17,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// ✅ Serve uploaded images publicly
+// Serve uploaded images publicly
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Test route
@@ -28,6 +29,7 @@ app.get('/', (req, res) => {
 app.use('/api/auth',   authRoutes);
 app.use('/api/admin',  adminRoutes);
 app.use('/api/pizzas', pizzaRoutes);
+app.use('/api/orders', orderRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

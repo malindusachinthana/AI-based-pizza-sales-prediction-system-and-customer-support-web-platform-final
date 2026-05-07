42a5c60a189c37095e07fff4174b32f293b4640b
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-07 23:51:28 +0530

    imports-specialOffer.jsx

--- frontend/src/pages/client/specialOffers.jsx
+++ frontend/src/pages/client/specialOffers.jsx
@@ -1,3 +1,9 @@
 // SpecialOffers.jsx
 // Place in: frontend/src/pages/client/SpecialOffers.jsx
 
+import React, { useEffect, useState } from 'react';
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+import Chatbot from '../../components/chatbot.jsx';
+import '../../style/specialOffers.css';
+

2c015cfccdd96c1e3fac8c73cfc6eee5f223fa62
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-09 22:58:51 +0530

    import-img-gallery-jsx

--- frontend/src/pages/client/gallery.jsx
+++ frontend/src/pages/client/gallery.jsx
@@ -3,3 +3,25 @@ import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
 import Chatbot from '../../components/chatbot.jsx';
 import '../../style/gallery.css';
+
+// ── Image imports ─────────────────────────────────────────────
+import img01 from '../../assets/airpizzaimg.jpeg';
+import img02 from '../../assets/greenGarden pizzaimg.jpg';
+import img20 from '../../assets/home01.jpg';
+import img03 from '../../assets/home03.jpg';
+import img04 from '../../assets/home05.jpg';
+import img05 from '../../assets/home04.jpg';
+import img06 from '../../assets/rest_1.jpg';
+import img07 from '../../assets/rest_2.jpg';
+import img08 from '../../assets/rest_3.jpg';
+import img09 from '../../assets/rest_4.jpg';
+import img10 from '../../assets/rest_5.jpg';
+import img11 from '../../assets/rest_6.jpg';
+import img12 from '../../assets/rest_7.jpg';
+import img13 from '../../assets/rest01.jpg';
+import img14 from '../../assets/theClassicimg.jpg';
+import img15 from '../../assets/theChickenAlfimg.jpg';
+import img16 from '../../assets/theGreekimg.jpg';
+import img17 from '../../assets/woodfire_1.jpg';
+import img18 from '../../assets/woodfire_2.jpg';
+import img19 from '../../assets/woodfire_3.jpg';

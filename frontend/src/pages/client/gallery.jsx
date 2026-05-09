4d89e804e205ec445fbd87f3448083d27b5eb64a
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-09 22:58:19 +0530

    create-gallery.jsx

---
+++ frontend/src/pages/client/gallery.jsx
@@ -0,0 +1,5 @@
+import React, { useState, useEffect, useCallback } from 'react';
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+import Chatbot from '../../components/chatbot.jsx';
+import '../../style/gallery.css';

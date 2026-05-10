db82d08e0aa2e3624be35bc0e056324fb836b256
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-10 23:25:37 +0530

    image & next-gallery-jsx

--- frontend/src/pages/client/gallery.jsx
+++ frontend/src/pages/client/gallery.jsx
@@ -142,3 +142,29 @@ export default function Gallery() {
               <polyline points="15 18 9 12 15 6"/>
             </svg>
           </button>
+
+          {/* Image */}
+          <div className="gl-lb-img-wrap" onClick={e => e.stopPropagation()}>
+            <img
+              src={IMAGES[lightbox].src}
+              alt={IMAGES[lightbox].alt}
+              className="gl-lb-img"
+            />
+            <p className="gl-lb-caption">{IMAGES[lightbox].caption}</p>
+          </div>
+
+          {/* Next */}
+          <button className="gl-lb-nav gl-lb-nav--next" onClick={nextImg}>
+            <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2.5">
+              <polyline points="9 18 15 12 9 6"/>
+            </svg>
+          </button>
+
+        </div>
+      )}
+
+      <Footer />
+      <Chatbot />
+    </>
+  );
+}

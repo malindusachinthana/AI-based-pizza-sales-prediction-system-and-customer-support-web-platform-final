1b4923ab1cd4ff52a347127958f56c7af3e46240
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-10 23:33:19 +0530

    create & import-images-aboutUs.jsx

---
+++ frontend/src/pages/client/aboutUs.jsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Navbar from '../../components/navbar';
+import Footer from '../../components/footer';
+import Chatbot from '../../components/chatbot.jsx';
+import '../../style/aboutUs.css';
+
+import aboutPizza from '../../assets/mainGalleryimg.png';
+import heroBg     from '../../assets/MainBackgound.png';

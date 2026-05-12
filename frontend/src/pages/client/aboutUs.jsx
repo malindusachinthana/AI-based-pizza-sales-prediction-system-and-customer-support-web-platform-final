c7c05c4504ab9aa1bd142a2a6ed800cfb91f679a
Malindu Sachinthana <malinduuus@gmail.com>
2026-05-12 22:42:03 +0530

    import-aboutUs.jsx

--- frontend/src/pages/client/aboutUs.jsx
+++ frontend/src/pages/client/aboutUs.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../../components/navbar';
 import Footer from '../../components/footer';
